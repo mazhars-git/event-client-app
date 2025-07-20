65c6176b34162f4cd3f9e8c24bca8b60e1d18c50
mazhars-git <mazharulislam611@gmail.com>
2025-07-21 00:38:04 +0600

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Events from "./components/Events/Events";
+import Footer from "./components/Shared/Footer";
 
 function App() {
   return (
     <>
       <Events />
+      <Footer />
     </>
   );
 }
--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,8 +1,14 @@
 const Footer = () => {
   return (
-    <div>
-      <h1>This is Footer</h1>
-    </div>
+    <footer className="flex justify-around items-center bg-black text-white py-4">
+      <div className="flex gap-5">
+        <h1 className="font-bold">E</h1>
+        <p>&copy; All Rights Reserved: 2025</p>
+      </div>
+      <div>
+        <h1>Social Links</h1>
+      </div>
+    </footer>
   );
 };
 
