cf650e2dc2354828003d7819eddbf5b28b40cc23
mazhars-git <mazharulislam611@gmail.com>
2025-07-31 00:37:44 +0600

    update card design

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -24,41 +24,43 @@ interface EventCardProps {
 
 export default function EventCard({ data }: EventCardProps) {
   return (
-    <Card className="w-full max-w-md shadow-xl rounded-2xl border border-gray-300">
+    <Card className="w-full max-w-md shadow-2xl rounded-xl border border-gray-300 bg-cyan-800">
       <CardHeader>
-        <CardTitle className="text-xl font-semibold">{data.title}</CardTitle>
+        <CardTitle className="text-xl text-slate-300 font-semibold">
+          {data.title}
+        </CardTitle>
         <CardDescription className="text-sm text-muted-foreground">
           {data.date} • {data.time}
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-3">
-        <div className="text-sm text-gray-700">
+        <div className="text-sm text-slate-300">
           <span className="font-medium">Notes:</span> {data.notes}
         </div>
-        <div className="inline-block px-2 py-1 text-xs font-medium bg-blue-100 text-blue-800 rounded-full">
+        <div className="inline-block px-2 py-1 text-xs font-medium bg-slate-200 text-blue-800 rounded-full">
           {data.category}
         </div>
         <div className="text-center space-x-2">
           <Link to="/update-event">
             <Button
-              variant="outline"
+              variant="ghost"
               size="sm"
-              className="bg-blue-400 text-white mt-2"
+              className="bg-sky-500 text-white mt-2"
             >
               Update
             </Button>
           </Link>
           <Button
-            variant="outline"
+            variant="ghost"
             size="sm"
             className="bg-red-400 text-white mt-2"
           >
             Delete
           </Button>
           <Button
-            variant="outline"
+            variant="ghost"
             size="sm"
-            className="bg-gray-400 text-white mt-2"
+            className="bg-green-500 text-white mt-2"
           >
             Archive
           </Button>
--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -17,7 +17,7 @@ const Events = () => {
   return (
     <div className="container mx-auto min-h-screen">
       <h1 className="text-3xl text-center py-4 font-bold">Upcoming Events</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-3">
         {events.map((event) => (
           <EventCard data={event} />
         ))}
